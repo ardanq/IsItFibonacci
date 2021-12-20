02a36b1538c21cabdec0e3629f3bf6042007cc7c
Ardan Öz <ardan.oz@outlook.com>
2021-12-20 16:42:56 +0300

    Add project files.

---
+++ IsItFıbonacci/FibonacciNumbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItFıbonacci
+{
+    public  class FibonacciNumbers
+    {
+        public static void GetFibonacci(int n)
+        {
+            int number = n - 1;
+            int[] Fib = new int[n];
+            Fib[0] = 0;
+            Fib[1] = 1;
+            
+
+            for (int i = 2; i <= number; i++)
+            {
+                Fib[i] = Fib[i - 2] + Fib[i - 1];
+            }
+
+
+            //indexof returns -1 when array doesn't contain n
+
+            int index = Array.IndexOf(Fib, n);
+
+
+            if (index != -1)
+            {
+                Console.WriteLine("yes");
+                
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ IsItFıbonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItFıbonacci
+{
+    internal class Program:FibonacciNumbers
+    {
+        static void Main(string[] args)
+        {
+            GetFibonacci(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine();
+
+            Console.Read();
+
+        }
+    }
+}
